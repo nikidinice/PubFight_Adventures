999fcab02ddc08fa6b9e31af8f5c0adbcb04b999
Nikita Samorokov <nikidinice@gmail.com>
2023-09-18 16:09:39 +0200

    v0.05

---
+++ src/main/kotlin/PubFightAdventures/Boss.kt
@@ -0,0 +1,4 @@
+package PubFightAdventures
+
+class Boss {
+}
---
+++ src/main/kotlin/PubFightAdventures/functions.kt
@@ -0,0 +1,13 @@
+package PubFightAdventures
+
+/**
+ * Hier bündel ich alle Funktionen, die zum Spiel gehören, um die Übersichtlichkeit zu gewährleisten
+ */
+
+//TODO: Background Functions
+
+fun clearConsole() {
+    for (i in 1..50) {
+        println("")
+    }
+}
